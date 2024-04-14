e2cc49491c37d3246c2ac099fd5623dc81743751
Tomáš Polák <polak.tomas01@gmail.com>
2024-04-14 14:36:52 +0200

    change font

--- scripts/SpeechContainer.gd
+++ scripts/SpeechContainer.gd
@@ -1,6 +1,6 @@
 extends Sprite
 
-var button_icon = preload("res://textures/ui_dialog_arrow.png")
+var button_icon = preload ("res://textures/ui_dialog_arrow.png")
 
 onready var mayor_image = $MayorControl/MayorImage
 onready var mayor_label = $MayorControl/MayerLabel
@@ -14,15 +14,14 @@ onready var how_button = $MinerControl/HowButton
 onready var k_button = $MinerControl/KButton
 onready var neh_button = $MinerControl/NehButton
 
-
-const mayor_intro_text = "Dear miner, you have been assigned the utmost\nurgent task. We need you to go and prepare\na way for our brand new SUBWAY."
-const mayor_tut_1 = "It’s quite simple, really. All you have to do is\nto go underground and place as  many rails as\nyou can before the subway arrives."
-const mayor_tut_2 = "You can walk using WASD / arrow keys.\nWalking towards breakable stones\nalso works as mining them."
-const mayor_tut_3 = "Furthermore, you can place rails\nusing the F key near the last open rail.\nHolding the F key on a placed rail removes it."
+const mayor_intro_text = "Dear miner, you have been assigned the utmost urgent task. We need you to go and prepare a way for our brand new SUBWAY."
+const mayor_tut_1 = "It’s quite simple, really. All you have to do is to go underground and place as  many rails as you can before the subway arrives."
+const mayor_tut_2 = "You can walk using WASD / arrow keys. Walking towards breakable stones also works as mining them."
+const mayor_tut_3 = "Furthermore, you can place rails using the F key near the last open rail. Holding the F key on a placed rail removes it."
 const mayor_tut_4 = "All clear?"
 const mayor_ok_text = "Great, now get to work, the subway is ready to go in a while."
-const mayor_train_crashed_start = "You did what you could and that’s what counts.\nYou managed to place "
-const mayor_train_crashed_finish = " brno-miles of rails\nbefore your impending doom. Wanna try again?"
+const mayor_train_crashed_start = "You did what you could and that’s what counts. You managed to place "
+const mayor_train_crashed_finish = " brno-miles of rails before your impending doom. Wanna try again?"
 const mayor_pause_speech = "What?! Aren't you supposed to be digging?"
 
 const miner_text = ". . ."
@@ -91,7 +90,7 @@ func _k_button_pressed() -> void:
 		_start_game()
 		
 func _nah_button_pressed() -> void:
-	_exit_game()	
+	_exit_game()
 
 func _push_selected_button() -> void:
 	print(selected_button_index)
@@ -189,20 +188,20 @@ func _set_mayor_tut_2_speech() -> void:
 	
 func _set_mayor_tut_3_speech() -> void:
 	$MajorTalk.play()
-	mayor_text_label.visible_characters = 0	
+	mayor_text_label.visible_characters = 0
 	mayor_text_label.text = mayor_tut_3
 	mayor_active_text = mayor_tut_3
 	$MayorControl/MayorText/MayorTextTimer.start()
 	
 func _set_mayor_tut_4_speech() -> void:
-	$MajorTalk.play()	
-	mayor_text_label.visible_characters = 0	
+	$MajorTalk.play()
+	mayor_text_label.visible_characters = 0
 	mayor_text_label.text = mayor_tut_4
 	mayor_active_text = mayor_tut_4
 	$MayorControl/MayorText/MayorTextTimer.start()
 	
 func _set_mayor_train_crash_speech(score) -> void:
-	$MajorTalk.play()	
+	$MajorTalk.play()
 	mayor_text_label.visible_characters = 0
 	mayor_text_label.text = mayor_train_crashed_start + str(score) + mayor_train_crashed_finish
 	mayor_active_text = mayor_text_label.text
@@ -236,7 +235,7 @@ func _on_SpeechContainer_mayor_tut_4_finished() -> void:
 	_set_miner_speech()
 
 func _on_Train_train_finished(score) -> void:
-	yield(get_tree().create_timer(2), "timeout")
+	yield (get_tree().create_timer(2), "timeout")
 	game_finished = true
 	self.visible = true
 	_set_mayor_train_crash_speech(score)
